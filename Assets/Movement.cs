c8dd605288faf77d80479df566507cf0fb060855
Necaja <nikitaspiridonov5@gmail.com>
2022-09-29 19:30:42 +0500

    Project
    first attemt

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+    public static float vertical, horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
+    }
+}
---
+++ Assets/jump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    bool is_ground = false;
+    Rigidbody Capsule;
+    public float force = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Capsule = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "ground") is_ground = true; 
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "ground") is_ground = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && is_ground)
+            Capsule.AddForce(Vector2.up * force, ForceMode.Impulse);
+    }
+}
