437fc13900801300adda8adf540eb2715b67e28f
Necaja <nikitaspiridonov5@gmail.com>
2022-10-01 14:58:59 +0500

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,11 +7,11 @@ public class Movement : MonoBehaviour
     public float movementSpeed = 10;
     public float turningSpeed = 60;
     public static float vertical, horizontal;
-
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
