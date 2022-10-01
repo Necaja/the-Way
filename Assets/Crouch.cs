00d91db6f25f1d4a5a05156e2adf868888595a6f
Necaja <nikitaspiridonov5@gmail.com>
2022-10-02 02:20:55 +0500

    экспорт кодов

---
+++ Assets/Crouch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) | Input.GetKeyDown(KeyCode.C))
+            gameObject.transform.localScale -= new Vector3(0, 0.5f, 0);
+
+        if (Input.GetKeyUp(KeyCode.LeftControl) | Input.GetKeyUp(KeyCode.C))
+            gameObject.transform.localScale += new Vector3(0, 0.5f, 0);    
+    }
+}
