d42c5d42d01cb1d5b3185c234933059b3dd442b1
Necaja <nikitaspiridonov5@gmail.com>
2022-10-03 18:32:59 +0500

    о

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,17 +7,15 @@ public class Movement : MonoBehaviour
     public float movementSpeed = 10;
     public float turningSpeed = 60;
     public static float vertical, horizontal;
-    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        animator.GetComponent<Animator>();
+        
     }
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
         transform.Translate(0, 0, horizontal);
-        
     }
 }
---
+++ Assets/Use.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Use : MonoBehaviour
+{
+    bool can_use = false;
+    public GameObject Usable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player") can_use = true; 
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player") can_use = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.E) && can_use)
+         Destroy(gameObject);
+    }
+}
